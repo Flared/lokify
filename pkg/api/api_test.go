5a73f390d26a602feb4d294523508ef13c186265
Samuel Laroche <samuel.laroche@flare.systems>
2021-09-29 11:29:28 -0400

    re-organized api and loki client

--- pkg/api/api_test.go
+++ pkg/api/api_test.go
@@ -1,30 +1,32 @@
 package api_test
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"reflect"
 	"strings"
 	"testing"
 
 	"github.com/flared/lokify/pkg/api"
-	"github.com/flared/lokify/pkg/api/appctx"
-	"github.com/flared/lokify/pkg/loki"
 )
 
-type lokiClientTest struct {
-	queryReturn *loki.QueryResponse
+type lokiQueryResponse struct {
+	Status string        `json:"status"`
+	Data   lokiQueryData `json:"data"`
 }
 
-func (client lokiClientTest) Query(query string) (*loki.QueryResponse, error) {
-	return client.queryReturn, nil
+type lokiQueryData struct {
+	ResultType string                `json:"resultType"`
+	Result     []lokiQueryDataResult `json:"result"`
 }
 
-func newLokiClientTest(queryReturn *loki.QueryResponse) *lokiClientTest {
-	return &lokiClientTest{
-		queryReturn: queryReturn,
-	}
+type lokiQueryDataResult struct {
+	Stream map[string]string `json:"stream"`
+	Values [][2]string       `json:"values"`
 }
 
 type apiTest struct {
@@ -36,8 +38,9 @@ func (apiTest *apiTest) Close() {
 	apiTest.server.Close()
 }
 
-func newApiTest(t *testing.T, lokiClient *lokiClientTest) *apiTest {
-	ctx := appctx.New(lokiClient)
+func newApiTest(t *testing.T, lokiServer *httptest.Server) *apiTest {
+	ctx := api.NewContext(lokiServer.Client(), lokiServer.URL)
+
 	return &apiTest{
 		server: httptest.NewServer(api.NewRouter(ctx)),
 		t:      t,
@@ -45,21 +48,22 @@ func newApiTest(t *testing.T, lokiClient *lokiClientTest) *apiTest {
 }
 
 func TestApi(t *testing.T) {
-	apiTest := newApiTest(t, nil)
+	apiTest := newApiTest(t, &httptest.Server{})
 	defer apiTest.Close()
 
-	if _, err := http.Get(strings.Join([]string{apiTest.server.URL, "api/status"}, "/")); err != nil {
+	statusUrl := strings.Join([]string{apiTest.server.URL, "api/status"}, "/")
+	if _, err := http.Get(statusUrl); err != nil {
 		t.Fatal(err)
 	}
 
 }
 
 func TestQuery(t *testing.T) {
-	expected := &loki.QueryResponse{
+	respBody := &lokiQueryResponse{
 		Status: "success",
-		Data: loki.QueryData{
+		Data: lokiQueryData{
 			ResultType: "streams",
-			Result: []loki.QueryDataResult{
+			Result: []lokiQueryDataResult{
 				{
 					Stream: map[string]string{"label": "value"},
 					Values: [][2]string{{"123456789", "log message"}},
@@ -67,24 +71,42 @@ func TestQuery(t *testing.T) {
 			},
 		},
 	}
+	var respBodyBytes bytes.Buffer
+	if err := json.NewEncoder(&respBodyBytes).Encode(respBody); err != nil {
+		t.Fatal(err)
+	}
 
-	lokiClient := newLokiClientTest(expected)
+	lokiServer := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		rw.Write(respBodyBytes.Bytes())
+	}))
+	defer lokiServer.Close()
 
-	apiTest := newApiTest(t, lokiClient)
+	apiTest := newApiTest(t, lokiServer)
 	defer apiTest.Close()
 
-	queryUrl := strings.Join([]string{apiTest.server.URL, "api/query?query=%7Blabel%3D%22value%22%7D"}, "/")
+	query := `{label="value"}`
+
+	params := strings.Join(
+		[]string{
+			"query=" + url.QueryEscape(query),
+		},
+		"&",
+	)
+
+	queryUrl := apiTest.server.URL + "/api/query?" + params
+
+	fmt.Printf("query url, %v\n", queryUrl)
 
 	if resp, err := http.Get(queryUrl); err != nil {
 		t.Fatal(err)
 	} else {
 		defer resp.Body.Close()
 
-		var q loki.QueryResponse
+		var q lokiQueryResponse
 		if err := json.NewDecoder(resp.Body).Decode(&q); err != nil {
 			t.Fatal(err)
-		} else if !reflect.DeepEqual(&q, expected) {
-			t.Fatalf("got %+v, expect %+v", &q, expected)
+		} else if !reflect.DeepEqual(&q, respBody) {
+			t.Fatalf("got %+v, expect %+v", &q, respBody)
 		}
 	}
 }
